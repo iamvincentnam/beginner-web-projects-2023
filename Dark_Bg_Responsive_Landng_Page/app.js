7321021dc494af3163d69cbbbf32a440cc3fe64d
iamvincentnam <vincentnam22@gmail.com>
2023-09-12 17:20:17 +0100

    replaced the Hamburger icon with a hard coded one

--- Dark_Bg_Responsive_Landng_Page/app.js
+++ Dark_Bg_Responsive_Landng_Page/app.js
@@ -1,9 +1,17 @@
 const nav =document.querySelector('nav');
-const hamburgerIcon=  document.querySelector('#hamburger_img');
+// const hamburgerIcon=  document.querySelector('#hamburger_img');
+const hamburgerBtn=  document.querySelector('#hamburger_btn');
 
 const ul_links=  document.querySelector('.nav-links');
-hamburgerIcon.addEventListener('click',(e)=>{
+
+
+hamburgerBtn.addEventListener('click',(e)=>{
     ul_links.classList.toggle('toggle_nav_link_display');
-    console.log(hamburgerIcon)
+    hamburgerBtn.classList.toggle('hamburger_active');
+    // console.log(hamburgerIcon)
+    setTimeout(()=>{
+ul_links.classList.contains('toggle_nav_link_display') ? document.querySelector('.toggle_nav_link_display').style.opacity=1 : ul_links.style.opacity=0;
+
+    },10)
   
 });
